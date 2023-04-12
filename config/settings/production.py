b88d82ca93df44afd1d5f0f131819c059dacb2c8
behtash-fani <behtash.fani@gmail.com>
2023-04-12 12:28:33 +0330

    add csrf trusted origins in production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,5 +2,5 @@ from .base import *
 
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+CSRF_TRUSTED_ORIGINS = ['http://104.234.196.203']
