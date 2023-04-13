62973d7ad3147e142f858c089c615b36c5d174cb
behtash-fani <behtash.fani@gmail.com>
2023-04-13 03:36:33 +0330

    change docker compose file for production

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
         "PORT": 5432,
     }
 }
