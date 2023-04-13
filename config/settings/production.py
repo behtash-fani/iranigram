ee9355b97cbc7f729ec615e2aebd7298dec7daa6
behtash-fani <behtash.fani@gmail.com>
2023-04-14 02:14:41 +0330

    Update ALLOWED_HOSTS value.

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,8 +2,8 @@ from .base import *
 
 
 
-# ALLOWED_HOSTS = ['104.234.196.203']
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 CSRF_TRUSTED_ORIGINS = ['http://104.234.196.203']
 
 
