34b57c8dce3146ad57689e08ba02ecdc3598a3a4
behtash-fani <behtash.fani@gmail.com>
2023-04-16 03:18:45 +0330

    - Update CSRF_TRUSTED_ORIGINS value.

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,8 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '104.234.196.203']
-# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
-CSRF_TRUSTED_ORIGINS = ['http://104.234.196.203']
+CSRF_TRUSTED_ORIGINS = 'http://104.234.196.203'
 DJANGO_SITE_URL = "http://104.234.196.203"
 
 STATIC_ROOT = BASE_DIR / "static"
