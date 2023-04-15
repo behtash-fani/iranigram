976e3990a0fbca648c0b03f2d1980732c882983f
behtash-fani <behtash.fani@gmail.com>
2023-04-16 02:59:25 +0330

    - Restrict ALLOWED_HOSTS to specific IPs.

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,7 +4,7 @@ from .base import *
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['104.234.196.203','127.0.0.1']
 # ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 CSRF_TRUSTED_ORIGINS = ['http://104.234.196.203']
 
