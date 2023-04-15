556446f10294d76ce8b913b3e485b9f1d463ceb8
behtash-fani <behtash.fani@gmail.com>
2023-04-16 02:27:09 +0330

    This commit updates the STATIC_URL and STATIC_ROOT variables in settings.py, removes the default NGINX configuration, and updates the static folder alias in the NGINX configuration.

--- config/settings/base.py
+++ config/settings/base.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 LOCALE_PATHS = (BASE_DIR / "templates/locale/",)
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Base url to serve media files
 MEDIA_URL = "/ticket_files/"
