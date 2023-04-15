1e9d3f747b4ffbc7d80a94b2753a0cb7c2d066e6
behtash-fani <behtash.fani@gmail.com>
2023-04-16 02:07:41 +0330

    The commit updates the gunicorn configuration file and the nginx Dockerfile to use the latest stable version and update the nginx.conf location.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 
-
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+# gunicorn config.wsgi:application --bind 0.0.0.0:8000
