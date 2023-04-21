34c01fad59772dec7986dcdade8457cdc4126fc1
behtash-fani <behtash.fani@gmail.com>
2023-04-21 04:29:57 +0330

    - Update template names for pages related to buying followers, likes, and views.

--- volumes/app/pages/views.py
+++ volumes/app/pages/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render
 from django.views import View
-import logging
-
-
-logger = logging.getLogger("my_logger")
 
 
 class HomeView(View):
-    template_name = 'pages/home.html'
+    template_name = "pages/home.html"
 
     def get(self, request):
-        logger.info("load home")
         return render(request, self.template_name)
-
 
 
 class FollowerView(View):
-    template_name = 'pages/buy_follower.html'
+    template_name = "pages/buy_follower.html"
+
     def get(self, request):
         return render(request, self.template_name)
+
 
 class LikeView(View):
-    template_name = 'pages/buy_like.html'
+    template_name = "pages/buy_like.html"
+
     def get(self, request):
         return render(request, self.template_name)
 
+
 class ViewView(View):
-    template_name = 'pages/buy_view.html'
+    template_name = "pages/buy_view.html"
+
     def get(self, request):
         return render(request, self.template_name)
