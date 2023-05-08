ed7b396f6818990be4daabfa83b3548fde15a1e6
behtash-fani <behtash.fani@gmail.com>
2023-05-08 15:28:43 +0330

    This commit includes updates to the logging configuration, navigation tabs, meta tags, button styling, and template services in Service.objects for improved user experience.

--- volumes/app/pages/views.py
+++ volumes/app/pages/views.py
@@ -7,12 +7,12 @@ class HomeView(View):
     template_name = "pages/home.html"
 
     def get(self, request):
-        template_service = Service.objects.filter(template_service=True).order_by('priority')
+        # template_service = Service.objects.filter(template_service=True).order_by('priority')
         follower_services = Service.objects.filter(template_service=True, template_service_category='follower').order_by('priority')
         like_services = Service.objects.filter(template_service=True, template_service_category='like').order_by('priority')
         view_services = Service.objects.filter(template_service=True, template_service_category='view').order_by('priority')
         context = {
-            'services' : template_service,
+            # 'services' : template_service,
             'follower_services': follower_services,
             'like_services': like_services,
             'view_services': view_services,
@@ -24,10 +24,10 @@ class FollowerView(View):
     template_name = "pages/buy_follower.html"
 
     def get(self, request):
-        template_service = Service.objects.filter(template_service=True).order_by('priority')
+        # template_service = Service.objects.filter(template_service=True).order_by('priority')
         follower_services = Service.objects.filter(template_service=True, template_service_category='follower').order_by('priority')
         context = {
-            'services' : template_service,
+            # 'services' : template_service,
             'follower_services': follower_services,
         }
         return render(request, self.template_name, context)
@@ -37,10 +37,10 @@ class LikeView(View):
     template_name = "pages/buy_like.html"
 
     def get(self, request):
-        template_service = Service.objects.filter(template_service=True).order_by('priority')
+        # template_service = Service.objects.filter(template_service=True).order_by('priority')
         like_services = Service.objects.filter(template_service=True, template_service_category='like').order_by('priority')
         context = {
-            'services' : template_service,
+            # 'services' : template_service,
             'like_services': like_services,
         }
         return render(request, self.template_name, context)
@@ -50,10 +50,10 @@ class ViewView(View):
     template_name = "pages/buy_view.html"
 
     def get(self, request):
-        template_service = Service.objects.filter(template_service=True).order_by('priority')
+        # template_service = Service.objects.filter(template_service=True).order_by('priority')
         view_services = Service.objects.filter(template_service=True, template_service_category='view').order_by('priority')
         context = {
-            'services' : template_service,
+            # 'services' : template_service,
             'view_services': view_services,
         }
         return render(request, self.template_name, context)
