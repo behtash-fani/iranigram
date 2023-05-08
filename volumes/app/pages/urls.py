c2aba143211da82808dd382d5a395705fbca0e4c
behtash-fani <behtash.fani@gmail.com>
2023-05-08 05:33:28 +0330

    - Update the URLs for Iranian Instagram followers, likes and views.

--- volumes/app/pages/urls.py
+++ volumes/app/pages/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'pages'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('iranian-instagram-followers', views.FollowerView.as_view(), name='follower'),
-    path('iranian_instagram_like', views.LikeView.as_view(), name='like'),
-    path('خرید-بازدید-ایرانی-اینستاگرام', views.ViewView.as_view(), name='view'),
+    path('iranian-instagram-followers/', views.FollowerView.as_view(), name='follower'),
+    path('iranian_instagram_like/', views.LikeView.as_view(), name='like'),
+    path('خرید-بازدید-ایرانی-اینستاگرام/', views.ViewView.as_view(), name='view'),
 ]
