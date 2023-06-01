db062d443f04c19786924a1401a9ddb367ae4b50
behtash-fani <behtash.fani@gmail.com>
2023-06-01 18:19:22 +0330

    update default.conf, buy_like.html, urls.py and footer.html

--- volumes/app/pages/urls.py
+++ volumes/app/pages/urls.py
@@ -5,6 +5,6 @@ app_name = 'pages'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('iranian-instagram-followers/', views.FollowerView.as_view(), name='follower'),
-    path('iranian_instagram_like/', views.LikeView.as_view(), name='like'),
+    path('خرید-لایک-ایرانی-اینستاگرام/', views.LikeView.as_view(), name='like'),
     path('خرید-بازدید-ایرانی-اینستاگرام/', views.ViewView.as_view(), name='view'),
 ]
