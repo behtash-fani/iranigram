14339388a5e2f542462f36f3a679f775305cfcfb
behtash-fani <behtash.fani@gmail.com>
2023-09-18 19:37:16 +0330

    improve seo manager for pages

--- volumes/app/seo/models.py
+++ volumes/app/seo/models.py
@@ -6,7 +6,8 @@ from django.utils.translation import gettext_lazy as _
 
 class PagesSeo(models.Model):
     page = models.CharField(_('Which Page'),max_length=255, blank=True, null=True)
-    meta_description_tag = models.TextField(_('Meta Description Tag'),blank=True, null=True)
+    description_tag = models.TextField(_('Meta Description Tag'),blank=True, null=True)
+    keywords_tag = models.CharField(_('Meta Keywords Tag'),max_length=255, blank=True, null=True)
     title_tag = models.CharField(_('Title Tag'),max_length=255, blank=True, null=True)
 
 
