80c6d9f8dcef0c509fa31e3efb592caf328c85ee
behtash-fani <behtash.fani@gmail.com>
2023-12-08 18:36:06 +0330

    make comments system

--- volumes/app/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('status/', views.StatusOrderView.as_view()),
-    path('add/', views.AddOrderView.as_view()),
-    path('balance/', views.UserBalanceView.as_view())
-]
--- volumes/app/comments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-
-app_name = 'comments'
-urlpatterns = [
-    path("list", views.submit_comment, name='comments_list')
-]
