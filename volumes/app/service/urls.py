f2c4754c752354858222fc44813510db7090a7d8
behtash-fani <behtash.fani@gmail.com>
2023-12-13 07:37:17 +0330

    make some beautification for comments system

--- volumes/app/service/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'services'
-urlpatterns = [
-    path('list/', views.service_list, name='service_list'),
-    path('service/desc/', views.get_description, name='service_desc'),
-]
