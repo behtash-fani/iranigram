a6b1d50b55f68b6afeb3f1459e5318db3db13e88
behtash-fani <behtash.fani@gmail.com>
2024-02-22 19:30:25 +0000

    add analytics of google

--- volumes/app/config/urls.py
+++ volumes/app/config/urls.py
@@ -43,13 +43,16 @@ urlpatterns = [
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap",
     ),
-    path("robots.txt/", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
-    
+    path("robots.txt/", TemplateView.as_view(template_name="robots.txt",
+         content_type="text/plain")),
+
 ]
 
 handler404 = 'pages.views.handling_404'
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
